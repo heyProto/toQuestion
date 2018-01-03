b36103a10d184829d054bbd0cb2a3489bdc0b516
Moiz Mansur Bookwala <moizmansur@gmail.com>
2018-01-03 16:07:29 +0530

    Makes as per new design without prev and next functionality

--- src/js/card.jsx
+++ src/js/card.jsx
@@ -48,65 +48,68 @@ export default class toQuestion extends React.Component {
           });
         }));
     } else {
-      this.componentDidUpdate();
+      //this.componentDidUpdate();
     }
   }
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      readMore: false
-    });
-    this.componentDidUpdate();
-  }
+  /* componentWillReceiveProps(nextProps) {
+   *   this.setState({
+   *     readMore: true
+   *   });
+   *   this.componentDidUpdate();
+   * }
+   */
+  /* componentDidUpdate() {
+   *   this.multiLineTruncate(document.querySelector('.protograph-toQuestion-description'));
+   * }
 
-  componentDidUpdate() {
-    this.multiLineTruncate(document.querySelector('.protograph-toQuestion-description'));
-  }
+   * multiLineTruncate(el) {
+   *   let data = this.state.dataJSON.data,
+   *       wordArray = data.description.split(' '),
+   *       props = this.props;
 
-  multiLineTruncate(el) {
-    let data = this.state.dataJSON.data,
-      wordArray = data.description.split(' '),
-      props = this.props;
-
-    while (el.scrollHeight > el.offsetHeight) {
-      wordArray.pop();
-      el.innerHTML = `${wordArray.join(' ')}... <br><button id="read-more-button" class="protograph-read-more">Keep reading</button>`;
-    }
-    if (document.getElementById('read-more-button') !== null) {
-      document.getElementById('read-more-button').addEventListener('click', (e) => {
-        document.getElementById('read-more-button').style.display = 'none';
-        document.querySelector('.protograph-toQuestion-description').style.maxHeight = 'none';
-        document.querySelector('.protograph-toQuestion-description').style.marginBottom = '10px';
-        document.querySelector('.protograph-toQuestion-description').innerHTML = data.description;
-        this.setState({
-          readMore: true
-        });
-        if (typeof props.clickCallback === 'function') {
-          props.clickCallback();
-        }
-      })
-    }
-  }
-
+   *   while (el.scrollHeight > el.offsetHeight) {
+   *     wordArray.pop();
+   *     el.innerHTML = `${wordArray.join(' ')}... <br><button id="read-more-button" class="protograph-read-more">Keep reading</button>`;
+   *   }
+   *   if (document.getElementById('read-more-button') !== null) {
+   *     document.getElementById('read-more-button').addEventListener('click', (e) => {
+   *       document.getElementById('read-more-button').style.display = 'none';
+   *       document.querySelector('.protograph-toQuestion-description').style.maxHeight = 'none';
+   *       document.querySelector('.protograph-toQuestion-description').style.marginBottom = '10px';
+   *       document.querySelector('.protograph-toQuestion-description').innerHTML = data.description;
+   *       this.setState({
+   *         readMore: true
+   *       });
+   *       if (typeof props.clickCallback === 'function') {
+   *         props.clickCallback();
+   *       }
+   *     })
+   *   }
+   * }
+   */
   renderLaptop() {
     if (this.state.fetchingData ){
       return(<div>Loading</div>)
     } else {
       let data = this.state.dataJSON.data;
-      let max_height = !this.state.readMore ? { maxHeight: "100px" } : { maxHeight: "none" }
       return (
         <div id="protograph_div" className="protograph-laptop-mode">
           <div className="protograph-card">
             <div className="protograph-toQuestion-question-container">
-              <h3 className="ui header protograph-toQuestion-question">{data.question}</h3>
-              <div>
-                <span className="protograph-toQuestion-score">{data.score}</span>
-                <span className="protograph-toQuestion-slash">/</span>
-                <span className="protograph-toQuestion-total">{data.total}</span>
+              <div className="ui grid">
+                <div className="seven wide column">
+                  <h1 className="ui header protograph-toQuestion-question">How is {data.company} performing on {data.element}?</h1>
+                  <div>
+                    <p className="protograph-toQuestion-description">{data.description}</p>
+                  </div>
+                </div>
+                <div className="nine wide column">
+                  <div className="protograph-toQuestion-sub-questions">
+                    {this.subQuestion(data.subQuestions[0].question, data.subQuestions[0].answer)}
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="protograph-toQuestion-description-container">
-              <p className="protograph-toQuestion-description" style={max_height}>{data.description}</p>
             </div>
           </div>
         </div>
@@ -114,13 +117,28 @@ export default class toQuestion extends React.Component {
     }
   }
 
+  subQuestion(question, answer, id) {
+    return(
+      <div className="protograph-toQuestion-sub-question">
+        <div className="protograph-toQuestion-sub-question-question">{question}</div>
+        <div className="protograph-toQuestion-sub-question-answer">{answer}</div>
+        <div className="protograph-toQuestion-sub-question-toggle-container">
+          <div className="protograph-toQuestion-sub-question-toggle prev">
+            &lt; Prev
+          </div>
+          <div className="protograph-toQuestion-sub-question-toggle next">
+            Next &gt;
+          </div>
+        </div>
+      </div>
+    )
+  }
 
   renderMobile() {
     if ( this.state.fetchingData ){
       return(<div>Loading</div>)
     } else {
       let data = this.state.dataJSON.data;
-      let max_height = !this.state.readMore ? { maxHeight: "100px" } : { maxHeight: "none" }
 
       return (
         <div id="protograph_div" className="protograph-mobile-mode">
